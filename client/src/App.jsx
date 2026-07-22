a6aa0a48f7f9134bc9dc741657239372ce09aa26
brianhw03 <brianhw03@gmail.com>
2026-07-23 01:15:24 +0700

    chore: clean up structure file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,43 +1,22 @@
-import { Navigate, Outlet, Routes, Route, useLocation } from "react-router";
+import { Routes, Route } from "react-router";
 import LandingPage from "./views/LandingPage/LandingPage";
 import Home from "./views/HomePage/Home";
 import CreateRoom from "./views/CreateRoom/CreateRoom";
-import Gameplay from "./views/Gameplay/Gameplay";
 import BaseLayout from "./layouts/BaseLayout";
-import { useGame } from "./context/GameContext";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route element={<GameNavigationGuard />}>
-          <Route path="/" element={<LandingPage />} />
+        <Route path="/" element={<LandingPage />} />
 
-          <Route element={<BaseLayout />}>
-            <Route path="/home" element={<Home />} />
-            <Route path="/room/:roomCode" element={<CreateRoom />} />
-            <Route path="/game/:roomCode" element={<Gameplay />} />
-          </Route>
+        <Route element={<BaseLayout />}>
+          <Route path="/home" element={<Home />} />
+          <Route path="/room/:roomCode" element={<CreateRoom />} />
         </Route>
       </Routes>
     </>
   );
-}
-
-function GameNavigationGuard() {
-  const location = useLocation();
-  const { activeGameRoomCode, username } = useGame();
-  const hasSession = Boolean(username);
-
-  if (
-    hasSession &&
-    activeGameRoomCode &&
-    location.pathname !== `/game/${activeGameRoomCode}`
-  ) {
-    return <Navigate to={`/game/${activeGameRoomCode}`} replace />;
-  }
-
-  return <Outlet />;
 }
 
 export default App;
